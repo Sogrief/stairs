216c3934e31bf31030d06d36fd859850e1afea37
Sogrief <charlesJD@outlook.fr>
2024-08-23 16:02:18 +0200

    ajout de la base du système de wall jump

--- scenes/player/scripts/player.gd
+++ scenes/player/scripts/player.gd
@@ -2,19 +2,23 @@ extends CharacterBody2D
 
 #constantes
 const SPEED : float = 300.0
-const JUMP_VELOCITY : float = -400.0
+const JUMP_VELOCITY : float = -550.0
 
 #autres variables
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity : int = 980
+var jump_count : int = 0
 
 func _physics_process(delta):
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() || jump_count > 0:
+		jump_count = 0
 		velocity.y = JUMP_VELOCITY
 
-	var direction = Input.get_axis("move_left", "move_right")
+	var direction : int = Input.get_axis("move_left", "move_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -22,11 +26,12 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+
+func _on_area_2d_body_entered(body):
 	if body is TileMap:
-		print("entered")
+		jump_count = 0
 
 
-func _on_area_2d_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+func _on_area_2d_body_exited(body):
 	if body is TileMap:
-		print("exited")
+		jump_count = 1
