ac4a4d1373165b98f306c19857c8c054d12cb372
Sogrief <charlesJD@outlook.fr>
2024-08-29 14:53:06 +0200

    déplacement dynamique du lanceur de projectiles

--- scenes/level/scripts/spawn_projectiles.gd
+++ scenes/level/scripts/spawn_projectiles.gd
@@ -3,13 +3,13 @@ extends Node2D
 @export var projectile : PackedScene
 @onready var projectile_launcher := %sprite_2d # lanceur de projectile
 @onready var path : Path2D = %path_2d # parcours 2D de la map
+@onready var path_follow : PathFollow2D = %path_follow_2d
 @onready var player : CharacterBody2D = %player # joueur
 
 var path_points : Array # l'ensemble des points du path2D
 var player_position : Vector2 # la position en temps réel du joueur
 var closest_point : Vector2 # point du path le plus proche du joueur
 var timer : float = 1.0 # le délai entre chaque projectile
-
 
 func _ready():
 	path_points = get_all_points_from_path(path) # récupération de tous les points du path2D
@@ -19,11 +19,10 @@ func _process(delta):
 	closest_point = path_points[get_closest_path_point(path_points, player_position)]
 	
 	var player_distance = path.curve.get_closest_offset(closest_point) #distance approximative parcourue
-	var progress_ratio = player_distance / path.curve.get_baked_length()
+	var progress_ratio = player_distance / path.curve.get_baked_length() #progress ratio approximatif du joueur
 	#print(path.curve.get_baked_length())
 	
-	
-	
+	path_follow.progress_ratio = progress_ratio + 0.2
 	
 	timer -= delta
 	
