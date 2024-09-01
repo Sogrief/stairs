bac5c33ce20f63e02a7ea08cbd7b4a05b64842fa
Sogrief <charlesJD@outlook.fr>
2024-09-01 15:55:50 +0200

    changement de la taille des colliders du projectile en fonction de sa taille dans l'enum

--- ressources/projectiles_size/projectile_size.gd
+++ ressources/projectiles_size/projectile_size.gd
@@ -1,6 +1,7 @@
 class_name projectile_size
 extends Resource
 
-@export var sprite_scale : float # taille du sprite 2D
+#@export var sprite_scale : float # taille du sprite 2D
+@export var area_collision_shape : Shape2D
 @export var collision_shape : Shape2D
 
--- scenes/projectiles/scripts/projectile.gd
+++ scenes/projectiles/scripts/projectile.gd
@@ -1,9 +1,35 @@
+@tool
 class_name projectile
 extends RigidBody2D
 
-signal player_collision
+@onready var sprite_2d = $sprite_2d
+@onready var collision_shape_2d = $collision_shape_2d
+@onready var area_collision_shape_2d = $area_2d/collision_shape_2d
 
 @export var projectile_size_array : Array[projectile_size] # tableau contenant des ressources de type projectile_size
+@export var size : SIZE:
+	set(value):
+		if value != size: # si la taille du projectile a changé
+			size = value
+			size_changed.emit()
+		
+signal player_collision
+signal size_changed
+
+enum SIZE {
+	SMALL = 0,
+	MEDIUM = 1,
+	BIG = 2
+}
+
+func _ready() -> void:
+	size_changed.connect(projectile_update_size)
+
+#------------------- mise à jour des propriétés du projectile -------------------
+func projectile_update_size() -> void:
+	#sprite_2d.scale = projectile_size_array[size].sprite_scale
+	collision_shape_2d.shape = projectile_size_array[size].collision_shape
+	area_collision_shape_2d.shape = projectile_size_array[size].area_collision_shape
 
 func _on_area_2d_body_entered(body):
 	if body is player:
