e81d2fd7c463c23806e8bdae7b6b79592267ee3d
Sogrief <charlesJD@outlook.fr>
2024-09-02 22:44:14 +0200

    fonctionnalité de jump en continu lors de la touche enfoncée

--- scenes/player/scripts/player.gd
+++ scenes/player/scripts/player.gd
@@ -16,7 +16,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("jump") and (is_on_floor() || jump_count > 0):
+	if Input.is_action_pressed("jump") and (is_on_floor() || jump_count > 0):
 		jump_count = 0
 		velocity.y = JUMP_VELOCITY
 
