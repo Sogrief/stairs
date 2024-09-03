7fe09587752a7fcda33f0149a22876982f58adcd
Sogrief <charlesJD@outlook.fr>
2024-09-03 21:22:46 +0200

    ajout d'un délai entre chaque dash

--- scenes/player/scripts/player.gd
+++ scenes/player/scripts/player.gd
@@ -1,8 +1,10 @@
 class_name player
 extends CharacterBody2D
 
-# constantes
 @onready var game_over_menu : CanvasLayer = %game_over_menu
+@onready var delay_between_dash = $delay_between_dash
+
+# constantes
 const SPEED : float = 300.0
 const JUMP_VELOCITY : float = -550.0
 
@@ -13,7 +15,7 @@ var wall_jump_sensitivity : int = 10 # velocité minimale en y pour pouvoir effe
 var double_tap_interval : float = 0.5 # délai en secondes entre deux pressions de touches pour détecter un dash
 var tap_count_right : int = 0 # nombre de fois que arrow_right a été pressé dans un cours labste de temps
 var tap_count_left : int = 0 # nombre de fois que arrow_left a été pressé dans un cours labste de temps
-var can_dash : float = false
+var can_dash : bool = true
 
 func _physics_process(delta):
 	#------------------- mouvements du personnage -------------------
@@ -35,17 +37,29 @@ func _physics_process(delta):
 	
 	#------------------- dash du personnage -------------------
 	if Input.is_action_just_pressed("move_left"):
-		tap_count_left += 1
-		
-		if tap_count_left == 2: # si c'est la deuxième fois que le joueur appuie
-			print(direction)
+		if can_dash == true:
+			tap_count_left += 1
+			
+			if tap_count_left == 2: # deuxième fois que le joueur appuie & délai dash pas en cours
+				delay_between_dash.start()
+				can_dash = false
+				dash(direction)
+				tap_count_left = 0
+				
+		else:
 			tap_count_left = 0
 		
 	if Input.is_action_just_pressed("move_right"):
-		tap_count_right += 1
-		
-		if tap_count_right == 2: # si c'est la deuxième fois que le joueur appuie
-			print(direction)
+		if can_dash == true:
+			tap_count_right += 1
+			
+			if tap_count_right == 2: # deuxième fois que le joueur appuie & délai dash pas en cours
+				delay_between_dash.start()
+				can_dash = false
+				dash(direction)
+				tap_count_right = 0
+				
+		else:
 			tap_count_right = 0
 	
 	if tap_count_left or tap_count_right == 1:
@@ -55,17 +69,18 @@ func _physics_process(delta):
 			double_tap_interval = 0.5
 			tap_count_right = 0
 			tap_count_left = 0
-
-func test(delta):
-	double_tap_interval -= delta
+			
 	
-	if double_tap_interval < 0:
-		double_tap_interval = 0.3
-		print("test")
+	#print(str(can_dash) + " | tap count left : " + str(tap_count_left) + " | tap count right : " + str(tap_count_right))
 
+#------------------- fonction de dash -------------------
+func dash(dash_direction):
+	print(dash_direction)
+
+#------------------- fonction de mort -------------------
 func death():
 	game_over_menu.show() # affiche le menu game over quand le joueur meurt
-
+	
 func _on_area_2d_body_entered(body):
 	if body is TileMap:
 		if velocity.y > wall_jump_sensitivity:
@@ -75,6 +90,10 @@ func _on_area_2d_body_exited(body):
 	if body is TileMap:
 		pass
 
+#fonction à mettre dans game manager plutôt ?
 func _on_main_child_entered_tree(node):
 	if node is projectile: # vérifie si un projectile est ajouté à la scène
 		node.player_collision.connect(death)
+
+func _on_delay_between_dash_timeout():
+	can_dash = true
