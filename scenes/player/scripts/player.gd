9444b38cbc0a6ab7fc1c8bd71be89de279e4347f
Sogrief <charlesJD@outlook.fr>
2024-09-03 20:35:27 +0200

    détection des double tap à gauche & à droite

--- scenes/player/scripts/player.gd
+++ scenes/player/scripts/player.gd
@@ -11,17 +11,36 @@ const JUMP_VELOCITY : float = -550.0
 var gravity : int = 980
 var jump_count : int = 0
 var wall_jump_sensitivity : int = 10 # velocité minimal en y pour pouvoir effectuer le prochain wall jump
-var double_tap_interval : float = 0.3 # délai en secondes entre deux pressions de touches pour détecter un dash
-var tap_count : int = 0 # nombre de pressions de touches successives
+var double_tap_interval : float = 0.5 # délai en secondes entre deux pressions de touches pour détecter un dash
+var tap_count_right : int = 0 # nombre de pressions de touches droite successives
+var tap_count_left : int = 0 # nombre de pressions de touches gauche successives
 
 func _physics_process(delta):
 	
 	#------------------- dash du personnage -------------------
-	double_tap_interval -= delta
+	#double_tap_interval -= delta
 	
-	if double_tap_interval < 0.0:
-		double_tap_interval = 0.3
-		print(delta)
+	if Input.is_action_just_pressed("move_left"):
+		tap_count_left += 1
+		
+		if tap_count_left == 2:
+			print("double tap left")
+			tap_count_left = 0
+		
+	if Input.is_action_just_pressed("move_right"):
+		tap_count_right += 1
+		
+		if tap_count_right == 2:
+			print("double tap right")
+			tap_count_right = 0
+	
+	if tap_count_left or tap_count_right == 1:
+		double_tap_interval -= delta
+		
+		if double_tap_interval < 0.0:
+			double_tap_interval = 0.5
+			tap_count_right = 0
+			tap_count_left = 0
 		
 	
 	#------------------- mouvements du personnage -------------------
@@ -40,6 +59,13 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func test(delta):
+	double_tap_interval -= delta
+	
+	if double_tap_interval < 0:
+		double_tap_interval = 0.3
+		print("test")
 
 func death():
 	game_over_menu.show() # affiche le menu game over quand le joueur meurt
