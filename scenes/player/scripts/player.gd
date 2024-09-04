8ce958e856cf9f371450056660ee11efa19ea8d4
Sogrief <charlesJD@outlook.fr>
2024-09-04 20:18:29 +0200

    dash progressif

--- scenes/player/scripts/player.gd
+++ scenes/player/scripts/player.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 
 # constantes
 const SPEED : float = 300.0
-const DASH_FORCE : float = 400.0
+const DASH_FORCE : float = 500.0
 const JUMP_VELOCITY : float = -550.0
 
 # autres variables
@@ -14,7 +14,8 @@ var gravity : int = 980
 var jump_count : int = 0
 var wall_jump_sensitivity : int = 10 # velocité minimale en y pour pouvoir effectuer le prochain wall jump
 @export var double_tap_interval : float = 0.5 # délai en secondes entre deux pressions de touches pour détecter un dash
-@export var dash_time : float = 0.8  #durée de dash
+@export var dash_duration : float = 0.8 # durée des dashs
+var dash_time : float # temps restant du dash en cours
 var tap_count_right : int = 0 # nombre de fois que arrow_right a été pressé dans un cours labste de temps
 var tap_count_left : int = 0 # nombre de fois que arrow_left a été pressé dans un cours labste de temps
 var can_dash : bool = true
@@ -36,8 +37,6 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	
 	#------------------- dash du personnage -------------------
 	if Input.is_action_just_pressed("move_left"):
 		if can_dash == true:
@@ -45,7 +44,7 @@ func _physics_process(delta):
 			
 			if tap_count_left == 2: # deuxième fois que le joueur appuie & délai dash pas en cours
 				delay_between_dash.start()
-				dash_time = 0.8
+				dash_time = dash_duration
 				dash_direction = direction
 				tap_count_left = 0
 				can_dash = false
@@ -59,7 +58,7 @@ func _physics_process(delta):
 			
 			if tap_count_right == 2: # deuxième fois que le joueur appuie & délai dash pas en cours
 				delay_between_dash.start()
-				dash_time = 0.8
+				dash_time = dash_duration
 				dash_direction = direction
 				tap_count_right = 0
 				can_dash = false
@@ -76,13 +75,19 @@ func _physics_process(delta):
 			tap_count_left = 0
 			
 	dash(dash_direction)
+	
+	move_and_slide()
+	
 
 #------------------- fonction de dash -------------------
 func dash(dash_direction):
-	if dash_time > 0:
-		dash_time -= get_process_delta_time()
-		velocity.x += DASH_FORCE * dash_direction
 
+	if dash_time > 0:
+		var dash_progress : float = dash_time / dash_duration
+		dash_time -= get_process_delta_time()
+		#velocity.x += DASH_FORCE * dash_direction
+		velocity.x += lerp(0.0, DASH_FORCE * dash_direction, dash_progress)
+		
 #------------------- fonction de mort -------------------
 func death():
 	game_over_menu.show() # affiche le menu game over quand le joueur meurt
